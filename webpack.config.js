98f3882961167ee88ecb0cd781a1e5f2c84669df
Tomasz Karczewski <tomasz.karczewski@ttpsc.pl>
2021-04-02 18:44:22 +0200

    Added og tags and banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const srcPath = path.resolve(__dirname, "src");
 const distPath = path.resolve(__dirname, "dist");
+const publicPath = path.resolve(__dirname, "public");
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
     host: "0.0.0.0",
-    contentBase: distPath,
+    contentBase: [distPath, publicPath],
     compress: true,
     port: 9000,
   },
@@ -19,6 +21,9 @@ module.exports = {
     clean: true,
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(srcPath, "index.html"),
     }),
